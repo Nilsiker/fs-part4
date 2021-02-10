19fd3fe2e0f15050ded7fcc0d8995522c4dc4227
Nilsiker <nilsiker94@gmail.com>
2021-02-10 15:56:50 +0100

    initial commit

---
+++ build/precache-manifest.afc8407a926a641f88441436d70d0b41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71cc2d180fb40607ad9d64aa4269afb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0972571dc789444198e3",
+    "url": "/static/css/main.9a883f2d.chunk.css"
+  },
+  {
+    "revision": "6fa96f4e77e54634d75a",
+    "url": "/static/js/2.89f8abb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89f8abb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0972571dc789444198e3",
+    "url": "/static/js/main.9f5edc3c.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
